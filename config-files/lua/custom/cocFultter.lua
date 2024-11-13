d4f0695b1eaee6c6eedf2881a02a1ed67b285400
Your Name <you@example.com>
2024-11-13 11:48:21 +0530

    code action remove is working but we not integrate the codelens rightnow

---
+++ config-files/lua/custom/cocFultter.lua
@@ -0,0 +1,10 @@
+local function remove_flutter_text_widget()
+  local current_line = vim.api.nvim_get_current_line()
+  if string.match(current_line, "Text%(%s*%)") then
+    vim.api.nvim_command("normal! dd")
+  end
+end
+
+vim.api.nvim_create_user_command("RemoveText", remove_flutter_text_widget, {})
+
+vim.api.nvim_set_keymap("n", "<leader>rt", ":RemoveText<CR>", { noremap = true, silent = true })
