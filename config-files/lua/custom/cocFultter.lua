123d234a7856aa2039c8fb1ad31dfd6fa7bd8fe0
Your Name <sibikrishna71@gmail.com>
2024-11-13 12:07:48 +0530

    Updated string match

--- config-files/lua/custom/cocFultter.lua
+++ config-files/lua/custom/cocFultter.lua
@@ -1,6 +1,6 @@
 local function remove_flutter_text_widget()
   local current_line = vim.api.nvim_get_current_line()
-  if string.match(current_line, "Text%(%s*%)") then
+  if string.match(current_line, "Text%(%s*.*%s*%)") then
     vim.api.nvim_command("normal! dd")
   end
 end
@@ -8,5 +8,3 @@ end
 vim.api.nvim_create_user_command("RemoveText", remove_flutter_text_widget, {})
 
 vim.api.nvim_set_keymap("n", "<leader>rt", ":RemoveText<CR>", { noremap = true, silent = true })
-
---This is code will Remoev the Text widget yes
